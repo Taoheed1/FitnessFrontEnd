35788672a18fc375ef3686699ea339f3eb2676c7
Taoheed1 <taoheed123afolayan@gmail.com>
2019-02-08 13:50:32 +0000

    CUD functionality

--- src/ProgramManager.js
+++ src/ProgramManager.js
@@ -8,17 +8,14 @@ class ProgramManager extends Component {
     constructor() {
         super();
         this.state = {
-            willCreate: "",
-            willUpdate: "",
-            willDelete: "",
-            optionPicked: false
+            ProgramManagerSelect: ""
         }
     }
 
     handleChange = (e) => {
-        this.setState = ({
+        this.setState({
             [e.target.id]: e.target.value
-        })
+        });
     }
 
     render() {
@@ -27,18 +24,17 @@ class ProgramManager extends Component {
                 <h1>What would you like to do?</h1>
                 <div class="form-group">
                     <label for="Manage Programs">Manage Programs</label>
-                    <select multiple="" class="form-control" id="ProficiencySelect" value={this.state.manageChoice} onChange={this.handleChange}>
-                        <option onSelect={this.handleChange}>Create Program</option>
-                        <option>Update Program</option>
-                        <option>Delete Program</option>
+                    <select multiple="" class="form-control" id="ProgramManagerSelect" onChange={this.handleChange}>
+                        <option value="">Select</option>
+                        <option value="CreateProgram">Create Program</option>
+                        <option value="UpdateProgram">Update Program</option>
+                        <option value="DeleteProgram">Delete Program</option>
                     </select>
                 </div>
-                <CreateProgram/>
-                <UpdateProgram />
-                {/* if(!this.props)
-                <CreateProgram willCreate={this.props.willCreate} />
-                <UpdateProgram willUpdate={this.props.willUpdate} />
-                <DeleteProgram willDelete={this.props.willDelete} /> */}
+                {this.state.ProgramManagerSelect === "CreateProgram" ?
+                    <CreateProgram /> : this.state.ProgramManagerSelect === "UpdateProgram" ?
+                        <UpdateProgram /> : this.state.ProgramManagerSelect === "DeleteProgram" ?
+                            <DeleteProgram /> : ""}
 
             </div>
         )
